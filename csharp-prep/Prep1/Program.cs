8a8c5fe1f72c98ed6099e01fbd5a8e2c55429f1c
bigblue9222 <devantueller3@gmail.com>
2023-09-14 12:54:59 -0600

    completed c Sharp prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String first = Console.ReadLine();
+        Console.WriteLine(first);
+        Console.Write("What is your last name? ");
+        String last = Console.ReadLine();
+        Console.WriteLine($"You name is {last},{first} {last}");
     }
-}
+
+}    
