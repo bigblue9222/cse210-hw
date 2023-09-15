fa57dcdbf04e4cc22d372ea4d850906912daf9ee
bigblue9222 <devantueller3@gmail.com>
2023-09-15 12:40:41 -0600

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    List<int> numbers = new List<int>();
+    int userNumnber = -2;
+    while (userNumnber != 0)
+    {
+      Console.Write("Enter a number (0 to quit): ");
+      string userResponse = Console.ReadLine();
+      userNumnber = int.Parse(userResponse);
+      if (userNumnber !=0)
+      {
+         numbers.Add(userNumnber);
+      }
+    }   
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+      sum += number;
     }
+    Console.WriteLine($"The sum is: {sum}");
+    int max = numbers[0];
+    foreach (int number in numbers)
+    {
+      if (number> max)
+      {
+         max = number;
+      }
+    }
+    Console.WriteLine($"The max is:{max}");     
+    }           
 }
